7a66f2e77621e9dde5578b1181289e99955960d0
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-06-14 17:03:42 +0000

    Posts/admin.py: Import Comment model.Define Admin Classes. Register all the models

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Category, Post
+from .models import Category, Post, Comment
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+# the class will be customized shortly
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+# the class will be customized shortly
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+# the class will be customized shortly
+
+
+
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
