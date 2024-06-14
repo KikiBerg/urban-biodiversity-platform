a1538dc29d24e69bc3b6ebce7bc43afc73d68501
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-06-14 17:33:07 +0000

    Posts/admin.py: customize the admin classes

--- posts/admin.py
+++ posts/admin.py
@@ -3,17 +3,23 @@ from .models import Category, Post, Comment
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
-# the class will be customized shortly
+    list_display = ('name', 'description')
+    search_fields = ('name', 'description')
 
 class PostAdmin(admin.ModelAdmin):
-    pass
-# the class will be customized shortly
+    list_display = ('title', 'author', 'created_at', 'status', 'category')
+    list_filter = ('status', 'category')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+   
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
-# the class will be customized shortly
-
+    list_display = ('post', 'author', 'content', 'created_at', 'status')
+    list_filter = ('status', 'created_at')
+    search_fields = ('author', 'content')
 
 
 
