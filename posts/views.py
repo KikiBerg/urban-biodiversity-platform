6e72de9f824ffa0e62bc88af8de4fdf160fcf60e
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-06-15 13:44:49 +0000

    posts/views.py: create the skeleton for the class-based views

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Category, Comment
+#from django.http import HttpResponse
 
 # Create your views here.
-def my_post(request):
-    return HttpResponse("Hello, this is a first test before deploying!")
+class PostListView(ListView):
+    pass
+# the class will be customized shortly
+
+
+class PostDetailView(DetailView):
+    pass
+# the class will be customized shortly
+
+
+class PostCreateView(CreateView):
+    pass
+# the class will be customized shortly
+
+
+class PostUpdateView(UpdateView):
+    pass
+# the class will be customized shortly
+
+
+class PostDeleteView(DeleteView):
+    pass
+# the class will be customized shortly
+
+
