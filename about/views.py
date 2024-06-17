81cec49a3a9e7ae8c23cd53b7cb02cd03a255d40
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-06-17 17:12:28 +0000

    About app: define view

--- about/views.py
+++ about/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+def about_contact(request):
+    """
+    Renders the About/Contact page
+    """
+    about = About.objects.all().order_by('-updated_at').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
