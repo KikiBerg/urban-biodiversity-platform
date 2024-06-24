c124b557d4d97578bcc3c9c138a201b3a7890d37
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-06-24 15:49:35 +0000

    About: add forms.py & update about_contact view

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render
-from .models import About
+from .models import About, ContactUs
+from .forms import ContactUsForm
 
 # Create your views here.
 def about_contact(request):
@@ -7,9 +9,16 @@ def about_contact(request):
     Renders the About/Contact page
     """
     about = About.objects.all().order_by('-updated_at').first()
+    contact_us_form = ContactUsForm
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+        "contact_us_form": contact_us_form
+        },
     )
+
+
+
+    
