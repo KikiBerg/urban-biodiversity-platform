77cf91765ccd8c8cc9363c0854a4268cb3cfec52
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-07-02 15:08:09 +0000

    Refine comprehensivensess between categ&posts in Django interface

--- posts/admin.py
+++ posts/admin.py
@@ -3,13 +3,26 @@ from .models import Category, Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Display a table of related posts within the category admin page.
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 0  # Number of empty forms to display
+    fields = ('title', 'author', 'status', 'created_at')
+    readonly_fields = ('created_at',)
+    show_change_link = True  # Adds a link to edit the post
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'status', 'created_by')
+    list_display = ('name', 'description', 'status', 'created_by','post_count')
     list_filter = ('status',)
-    search_fields = ('name', 'description')    
+    search_fields = ('name', 'description')
+    inlines = [PostInline]
+
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = 'Number of Posts'
 
 
 @admin.register(Post)
